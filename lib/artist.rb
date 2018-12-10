39094f7cec9fa4c2a5b854dff0fd4b9c2d969ea2
ktochow1 <ktochow1@binghamton.edu>
2018-12-10 19:28:38 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist 
+attr_accessor :name, :songs
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def new_song(name, genre)
+    name = Song.new
+    name.self = self
+  end
+end
